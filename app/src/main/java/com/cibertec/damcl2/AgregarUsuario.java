5e872ae2016161df5595de647164e91ac5fa099f
ctacuri <ctacuri@mnperu.com>
2020-11-23 19:36:15 -0500

    cambios menores

--- app/src/main/java/com/cibertec/damcl2/AgregarUsuario.java
+++ app/src/main/java/com/cibertec/damcl2/AgregarUsuario.java
@@ -1,8 +1,23 @@
 package com.cibertec.damcl2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.cibertec.damcl2.Helpers.UsersInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class AgregarUsuario extends AppCompatActivity {
 
@@ -10,5 +25,50 @@ public class AgregarUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.agregar_usuario);
+
+
+    }
+
+    public void registrar(View view) {
+        String nombres = ((EditText)findViewById(R.id.editTextTextPersonName)).getText().toString();
+        String apePaterno = ((EditText)findViewById(R.id.editTextTextPersonName2)).getText().toString();
+        String apeMaterno = ((EditText)findViewById(R.id.editTextTextPersonName3)).getText().toString();
+        String email = ((EditText)findViewById(R.id.editTextTextEmailAddress)).getText().toString();
+        String celular = ((EditText)findViewById(R.id.editTextPhone)).getText().toString();
+        String fechaNac = ((EditText)findViewById(R.id.editTextDate)).getText().toString();
+        String direccion = ((EditText)findViewById(R.id.editTextTextMultiLine)).getText().toString();
+        String ubigeo = ((EditText)findViewById(R.id.autoCompleteTextView)).getText().toString();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(UsersInterface.JSONURL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+
+        UsersInterface api = retrofit.create(UsersInterface.class);
+
+        Call<String> call = api.createNewUser(nombres, apeMaterno, apeMaterno, email, celular, fechaNac, direccion, ubigeo);
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                if (response.isSuccessful()) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(AgregarUsuario.this).create();
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage("El usuario fue registrado con éxito");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    Intent i = new Intent(AgregarUsuario.this,MainActivity.class);
+                                    startActivity(i);
+                                }
+                            });
+                    alertDialog.show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<String> call,@NonNull Throwable t) {}
+        });
     }
 }
--- app/src/main/java/com/cibertec/damcl2/Helpers/UsersInterface.java
+++ app/src/main/java/com/cibertec/damcl2/Helpers/UsersInterface.java
@@ -1,7 +1,12 @@
 package com.cibertec.damcl2.Helpers;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 
 public interface UsersInterface {
     String JSONURL = "https://artacuri.dev/api/Rest/";
@@ -11,4 +16,16 @@ public interface UsersInterface {
 
     @GET("Ubigeo")
     Call<String> getUbigeo();
+
+    @FormUrlEncoded
+    @POST("AddUsuario")
+    Call<String> createNewUser(@Field("nom") String nombre,
+                               @Field("pat") String apellidopa,
+                               @Field("mat") String apellidoma,
+                               @Field("ema") String email,
+                               @Field("cel") String celu,
+                               @Field("nac") String nac,
+                               @Field("dir") String dir,
+                               @Field("ubi") String ubi);
+
 }
